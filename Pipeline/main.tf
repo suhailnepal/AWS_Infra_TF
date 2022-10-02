5ae15683f413d3e4f50fdcc0e77dc6819e694a57
Suhail Nepal <suhailnepal@Suhail-G15.localdomain>
2022-10-02 18:46:35 +1100

    Update version

--- Pipeline/main.tf
+++ Pipeline/main.tf
@@ -4,11 +4,9 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>3.0"
+      version = ">=3.63.0"
     }
   }
-
-  required_version = "1.3.0"
 
    backend "s3" {
        bucket = "suhail-pipeline-test"
