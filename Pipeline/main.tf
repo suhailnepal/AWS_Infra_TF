117e500ef001fed559197d92174bb83afb9e11a4
Suhail Nepal <suhailnepal@Suhail-G15.localdomain>
2022-10-02 11:56:12 +1100

    Trigger pipeline

--- Pipeline/main.tf
+++ Pipeline/main.tf
@@ -1,17 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      Version = "~>3.27"
     }
   }
-  backend "s3" {
-    bucket         = "suhail-pipeline-test"
-    region         = "ap-southeast-2"
-}
+
+  required_version = ">=0.14.9"
+   backend "s3" {
+       bucket = "suhail-pipeline-test"
+       key    = "/"
+       region = "ap-southeast-2"
+   }
 }
 
 provider "aws" {
   version = "~>3.0"
-  region = "ap-southeast-2"
+  region  = "ap-southeast-2"
 }
