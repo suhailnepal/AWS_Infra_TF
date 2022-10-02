ebc3431441918eabe8f015bbd108f57e5d90f720
Suhail Nepal <suhailnepal@Suhail-G15.localdomain>
2022-10-02 11:42:18 +1100

    Cleanup code

--- Pipeline/main.tf
+++ Pipeline/main.tf
@@ -1,16 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
-
-  required_version = ">=0.14.9"
-
+  backend "s3" {
+    bucket         = "suhail-pipeline-test"
+    region         = "ap-southeast-2"
+}
 }
 
 provider "aws" {
+  region  = var.region
   version = "~>3.0"
-  region  = "ap-southeast-2"
 }
