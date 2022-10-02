cff7f5a12bae9f76bcd0a5c6db1d13e8712f950e
Suhail Nepal <suhailnepal@Suhail-G15.localdomain>
2022-10-02 22:00:46 +1100

    Cleanup provider

--- Pipeline/main.tf
+++ Pipeline/main.tf
@@ -6,29 +6,10 @@ provider "aws" {
 }
 
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>3.27"
-    }
-  }
-
-required_version = ">=1.3.0"
-
+  required_version = ">=1.3.0"
    backend "s3" {
        bucket = "suhail-pipeline-test"
+       key = "default"
        region = "ap-southeast-2"
    }
-}
-
-terraform {
-  required_version = ">0.14.7"
-  backend "s3" {}
-
-  required_providers {
-    aws = {
-      version = ">= 3.66.0"
-      source  = "hashicorp/aws"
-    }
-  }
 }
